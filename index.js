a0100c7893ad9516d3a346d6e73910ab1fb04e6b
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2024-04-22 15:52:29 +0900

    スパイペットのbotIdsのPresence設定を修正

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ client.on('ready', async () => {
     setInterval(async () => {
         botIds = await refreshSpyPetBotIdsDatabase();
     }, 1000 * 60);
-    
+    client.user.setPresence({ activities: [{ name: `on ${client.guilds.cache.size} servers | watching ${client.members.cache.size} members | and I know ${botIds.length} Spy.Pet Bots`,  type: 	ActivityType.Watching }]});
     setInterval(async () => {
         client.user.setPresence({ activities: [{ name: `on ${client.guilds.cache.size} servers | watching ${client.members.cache.size} members | and I know ${botIds.length} Spy.Pet Bots`,  type: 	ActivityType.Watching }]});
     }, 1000 * 20);
